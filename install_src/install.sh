397e879e5e33c26c74164a9f3518cec144098a86
lebashupian <lebashupian163@163.com>
2019-02-27 17:06:53 +0800

    d

---
+++ install_src/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+	echo "安装系统依赖的rpm包"
+	yum -y groupinstall "Development tools"
+	yum -y install readline-devel
+	yum -y install lrzsz
+	yum -y install openssl-devel
+	yum -y install gdbm-devel
+	yum -y install mysql-server
+	yum -y install mysql
+	yum -y install mysql-devel
+
+	echo 编译安装ruby
+	tar -zxvf ruby-2.5.3.tar.gz
+	cd ruby-2.5.3
+	mkdir /opt/ruby2.5.3
+	./configure --prefix=/opt/ruby2.5.3/
+	make && make install
+
+	echo "将ruby的bin路径加入系统环境变量"
+cat <<EOF>> /etc/profile
+export PATH=/opt/ruby2.5.3/bin:$PATH
+EOF
+source /etc/profile
+
+	echo "修改gem源的为ruby-china"
+	gem sources --remove https://rubygems.org/
+	gem sources --add https://gems.ruby-china.com
+
+	echo "安装gem的bundle"
+	gem install bundle
+
+	echo "使用bundle安装gem依赖库"
+	bundle install
+
+	echo "完成"
